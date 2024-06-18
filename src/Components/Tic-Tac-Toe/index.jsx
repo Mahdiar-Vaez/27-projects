9220cd95fab7e8769e1d1d49be5d0614eec47815
Mahdiar <mahdyarvaez@gmail.com>
2024-06-18 10:05:45 -0700

    update the structure

--- src/Components/Tic-Tac-Toe/index.jsx
+++ src/Components/Tic-Tac-Toe/index.jsx
@@ -71,7 +71,16 @@ export default function TicTacToe() {
         <Square value={squares[1]} onClick={() => handleClick(1)} />
         <Square value={squares[2]} onClick={() => handleClick(2)} />
       </div>
-      {/* Repeat the above div for each row */}
+      <div className="row">
+        <Square value={squares[3]} onClick={() => handleClick(3)} />
+        <Square value={squares[4]} onClick={() => handleClick(4)} />
+        <Square value={squares[5]} onClick={() => handleClick(5)} />
+      </div>
+      <div className="row">
+        <Square value={squares[6]} onClick={() => handleClick(6)} />
+        <Square value={squares[7]} onClick={() => handleClick(7)} />
+        <Square value={squares[8]} onClick={() => handleClick(8)} />
+      </div>
       <h1>{status}</h1>
     </div>
   )
